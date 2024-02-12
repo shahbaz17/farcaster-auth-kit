e052cf650dc7d538a644db5684e907d3f2ad8adf
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-02-12 15:04:29 +0530

    Farcaster demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "@farcaster/auth-kit/styles.css";
+import { AuthKitProvider, SignInButton, useProfile } from "@farcaster/auth-kit";
+
+const config = {
+  rpcUrl: "https://mainnet.optimism.io",
+};
+
+export const UserProfile = () => {
+  const {
+    isAuthenticated,
+    profile: {
+      fid,
+      username,
+      displayName,
+      pfpUrl,
+      bio,
+      custody,
+      verifications,
+    },
+  } = useProfile();
+  return (
+    <div className="UserProfile">
+      {isAuthenticated ? (
+        <>
+          <p>
+            Hello, {displayName}! Your fid is: {fid}
+          </p>
+          <img className="pfpImg" src={pfpUrl} alt="Profile" />
+          <br />
+          <code>
+            <a
+              href={`https://warpcast.com/${username}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              @{username}
+            </a>
+            <div className="bio">{bio}</div>
+            <div className="more-details">
+              <div>
+                Custody Address:{" "}
+                <a
+                  href={`https://optimistic.etherscan.io/address/${custody}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {custody}
+                </a>
+              </div>
+              <div>
+                Verifications:{" "}
+                <a
+                  href={`https://optimistic.etherscan.io/address/${verifications}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {verifications}
+                </a>
+              </div>
+            </div>
+          </code>
+        </>
+      ) : (
+        <p>You're not signed in.</p>
+      )}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthKitProvider config={config}>
+      <div className="App">
+        <header className="App-header">
+          <SignInButton />
+          <UserProfile />
+        </header>
+      </div>
+    </AuthKitProvider>
   );
 }
 
